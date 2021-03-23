c7e50eab90fb3e6f86d4ba06f4a6d4ffb193f531
fancky2019 <709737588@qq.com>
2021-03-23 15:47:22 +0800

    init

---
+++ fancky-service/src/main/java/com/fancky/service/FanckyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fancky.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanckyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanckyServiceApplication.class, args);
+    }
+
+}
---
+++ fancky-web/src/main/java/com/fancky/web/FanckyWebApplication.java
@@ -0,0 +1,13 @@
+package com.fancky.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanckyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanckyWebApplication.class, args);
+    }
+
+}
